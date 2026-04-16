0479cf22b9866f7dd676091be971f42b668d2841
megusato <megusato@C010422716.zipangu.cac.co.jp>
2026-04-16 14:00:24 +0900

    Question01~06 基礎問題の提出

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -8,7 +8,14 @@ public class Question03_1 {
 
 	public static void main(String[] args) {
 
-		// TODO:ここに実装
-		
+		int price = 30;
+		int totalBudget = 300;
+		//購入予算を意味する
+
+		int quantity = totalBudget / price;
+		//商品の個数
+
+		System.out.println("商品の購入数は" + quantity + "です。");
+
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_3.java
@@ -1,13 +1,35 @@
 package basic.question06;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question06_3 {
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		// TODO:ここに実装
-		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int amount = 0;
+		int price = 100;
+
+		System.out.println("鉛筆を購入しますか? ");
+		System.out.println("はい：０、いいえ：1＞");
+
+		String choice = reader.readLine();
+		int buyFlag = Integer.parseInt(choice);
+		//buyFlagが0の場合、繰り返す
+		while (buyFlag == 0) {
+			amount++;
+			System.out.println("購入する鉛筆の本数を 1 つ増やしますか？");
+			System.out.print("はい：０、いいえ：1＞");
+
+			choice = reader.readLine();
+			buyFlag = Integer.parseInt(choice);
+		}
+
+		System.out.println("購入した鉛筆の本数は" + amount + "本です。");
+		System.out.println("購入した鉛筆の合計金額は" + (amount * price) + "円です。");
+
 	}
 
 }
